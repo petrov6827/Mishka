c5f9062b4cceec56ce76f942ba086c29766b7f1e
Dmitry <petrov6827@gmail.com>
2018-08-08 00:32:15 +0300

    gulp настроен. начал писать стили для моб версии

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,25 @@
 var gulp        = require('gulp');
 var sass        = require('gulp-sass');
-var browserSync = require('browser-sync');
+var browserSync = require('browser-sync').create();
 
-gulp.task('sass', function() {
+gulp.task('sass', function() {           //преобразование scss ---> css
     return gulp.src('./sass/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('css'))
-        .pipe(browserSync.reload({stream: true}))
+        .pipe(browserSync.stream())
     });
 
-gulp.task('browser-sync', function() {
-    browserSync({
+gulp.task('browser-sync', function() {   //отображение в браузере, сервер-локальный
+    browserSync.init({
         server: {
-            baseDir: 'app'
+            baseDir: './'
         },
         notify: false
     });
 });
 
-gulp.task('watch', ['browser-sync', 'sass'], function() {
+gulp.task('watch', ['browser-sync', 'sass'], function() {  //отображение в терминале изменений scss, html, js файлов
+
     gulp.watch('./sass/*.scss', ['sass']);
     gulp.watch('./*.html', browserSync.reload);
     gulp.watch('./js/**/*.js', browserSync.reload);
