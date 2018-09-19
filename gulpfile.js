3edb156cda1fcdb02f265a403a6d1a876b4ef697
Dmitry <petrov6827@gmail.com>
2018-09-19 22:09:21 +0300

    Оптимизация *svgstore *postcss *autoprefixer *mqpacker *csso

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,41 @@
+
 var gulp        = require('gulp');
 var sass        = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+
+var rename      = require('gulp-rename');
+var svgstore    = require('gulp-svgstore');
+var svgmin      = require('gulp-svgmin');
+
+var plumber     = require('gulp-plumber');
+var postcss     = require('gulp-postcss');
+var autoprefixer= require('autoprefixer');
+var mqpacker    = require('mqpacker');
+var minify      = require('gulp-csso');
+var rename      = require('gulp-rename');
+
+
+
+gulp.task('sass', function() {
+    gulp.src('./sass/style.scss')
+    .pipe(plumber())                     //проверка ошибок
+    .pipe(sass())
+    .pipe(postcss([
+        autoprefixer({browsers: [
+            'last 1 version',
+            'last 2 Chrome versions',
+            'last 2 Firefox versions',
+            'last 2 Opera versions',
+            'last 2 Edge versions',
+                    ]}),
+        mqpacker({...})             
+        ]))
+    .pipe(gulp.dest('css'))
+     
+    .pipe(minify())                      //минификация
+    .pipe(rename('style.min.css'))      
+    .pipe(gulp.dest('css'));
+})
 
 gulp.task('sass', function() {           //преобразование scss ---> css
     return gulp.src('./sass/**/*.scss')
