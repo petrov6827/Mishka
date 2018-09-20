690b6dd86f4399be38ecae259e4b6e75c2421bcb
Dmitry <petrov6827@gmail.com>
2018-09-20 16:46:30 +0300

    PNG -->>  SVG

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ var svgmin      = require('gulp-svgmin');
 var plumber     = require('gulp-plumber');
 var postcss     = require('gulp-postcss');
 var autoprefixer= require('autoprefixer');
-var mqpacker    = require('mqpacker');
+var mqpacker    = require('css-mqpacker');
 var minify      = require('gulp-csso');
 var rename      = require('gulp-rename');
 
@@ -18,23 +18,25 @@ var rename      = require('gulp-rename');
 
 gulp.task('sass', function() {
     gulp.src('./sass/style.scss')
-    .pipe(plumber())                     //проверка ошибок
-    .pipe(sass())
-    .pipe(postcss([
-        autoprefixer({browsers: [
-            'last 1 version',
-            'last 2 Chrome versions',
-            'last 2 Firefox versions',
-            'last 2 Opera versions',
-            'last 2 Edge versions',
-                    ]}),
-        mqpacker({...})             
-        ]))
-    .pipe(gulp.dest('css'))
-     
-    .pipe(minify())                      //минификация
-    .pipe(rename('style.min.css'))      
-    .pipe(gulp.dest('css'));
+        .pipe(plumber())                     //проверка ошибок
+        .pipe(sass())
+        .pipe(postcss([
+            autoprefixer({browsers: [
+                'last 1 version',
+                'last 2 Chrome versions',
+                'last 2 Firefox versions',
+                'last 2 Opera versions',
+                'last 2 Edge versions',
+                        ]}),
+            mqpacker({
+                sort: true
+            })             
+            ]))
+        .pipe(gulp.dest('css'))
+         
+        .pipe(minify())                      //минификация
+        .pipe(rename('style.min.css'))      
+        .pipe(gulp.dest('css'));
 })
 
 gulp.task('sass', function() {           //преобразование scss ---> css
