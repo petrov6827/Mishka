040b592fb26c33e0a3af22dd69b7ca6c241a9694
Dmitry <petrov6827@gmail.com>
2018-10-10 22:09:44 +0300

    catalog

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
 
 
-var nav = document.querySelector('.menu');        //меню
-var navToggle = document.querySelector('.main-nav__toggle'); //открыть меню
-var navClose = document.querySelector('.main-nav__close');   //закрыть меню
+var nav = document.querySelector('.menu');                    //меню
+var navCatalog = document.querySelector('.menu--close')
+var navToggle = document.querySelector('.main-nav__toggle');  //открыть меню
+var navClose = document.querySelector('.main-nav__close');    //закрыть меню
 
-navToggle.addEventListener('click', function(event) {        //клик на тогл
+navToggle.addEventListener('click', function(event) {         //клик на тогл
     event.preventDefault();
     navToggle.classList.add('main-nav__toggle--hidden');      //добавить класс к тоглу
     navClose.classList.remove('main-nav__close--hidden');     //убрать класс у кнопки закрыть
-    nav.classList.add('menu--show');          //+ класс показывающий меню
+    nav.classList.add('menu--show');
+    navCatalog.classList.add('menu--show');                   //+ класс показывающий меню
 });
-
-navClose.addEventListener('click', function(event) {         //клик на закрыть
+ 
+navClose.addEventListener('click', function(event) {          //клик на закрыть
     event.preventDefault();
     navToggle.classList.remove('main-nav__toggle--hidden');   //убрать класс у тогла
-    navClose.classList.add('main-nav__close--hidden');        //добавить класс кнопке закрыть
-    //nav.classList.add('main-nav__items-hidden');             
-    nav.classList.remove('menu--show');            //убрать класс показывающий меню
+    navClose.classList.add('main-nav__close--hidden');        //добавить класс кнопке закрыть              
+    nav.classList.remove('menu--show');                       //убрать класс показывающий меню
+    navCatalog.classList.remove('menu--show'); 
 });
 
 
